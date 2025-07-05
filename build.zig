b17e399f7f0a0e1335e637714ebb980c85f7ccf5
lumi2021 <94462076+lumi2021@users.noreply.github.com>
2025-07-05 00:46:15 -0300

    Build: Fix module problems

--- build.zig
+++ build.zig
@@ -6,14 +6,14 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib_mod = b.addModule(.{
+    const module = b.addModule("realizer", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
 
 
-    const lib_unit_tests = b.addTest(.{ .root_module = lib_mod });
+    const lib_unit_tests = b.addTest(.{ .root_module = module });
     const run_unit_tests = b.addRunArtifact(lib_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
